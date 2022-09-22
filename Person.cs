7a02940fcd8a0de9dd15830fcfc8f9bdd5e59ae4
admin <admin@VTORAYA0>
2022-09-22 19:48:55 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityInheritProject
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+    }
+
+    //[Table("Users")]
+    public class User : Person
+    {
+        public int Code { set; get; }
+    }
+
+    //[Table("Admins")]
+    public class Admin : Person
+    {
+        public string? Login { set; get; }
+    }
+
+    public class UserContext : DbContext
+    {
+        public DbSet<Person> Persons { set; get; } = null!;
+        public DbSet<Admin> Admins { set; get; } = null!;
+        public DbSet<User> Users { set; get; } = null!; 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=UsersDb;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Admin>().ToTable("Admins");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EntityInheritProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UserContext userContext = new UserContext())
+            {
+                userContext.Database.EnsureDeleted();
+                userContext.Database.EnsureCreated();
+
+                Person person1 = new() { Name = "Tim" };
+                Person person2 = new() { Name = "Sam" };
+                userContext.Persons.AddRange(person1, person2);
+
+                User user1 = new() { Name = "Bob", Code = 100 };
+                User user2 = new() { Name = "Joe", Code = 200 };
+                userContext.Users.AddRange(user1, user2);
+
+                Admin admin1 = new() { Name = "Tom", Login = "tom admin" };
+                Admin admin2 = new() { Name = "Jim", Login = "jim admin" };
+                userContext.Admins.AddRange(admin1, admin2);
+
+                userContext.SaveChanges();
+            }
+
+            using (UserContext userContext = new UserContext())
+            {
+                var persons = userContext.Persons.ToList();
+                Console.WriteLine("Persons");
+                foreach (var person in persons)
+                    Console.WriteLine(person.Name);
+
+                var users = userContext.Users.ToList();
+                Console.WriteLine("\nUsers");
+                foreach (var user in users)
+                    Console.WriteLine($"{user.Name} {user.Code}");
+
+                var admins = userContext.Admins.ToList();
+                Console.WriteLine("\nAdmins");
+                foreach (var admin in admins)
+                    Console.WriteLine($"{admin.Name} {admin.Login}");
+            }
+        }
+    }
+}
